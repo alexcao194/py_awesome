566c59a069c053a9c3780f824829c7f93f516ca4
Cao Việt Đức <112269340+alexcao194@users.noreply.github.com>
2024-03-15 00:24:43 +0700

    first commit

---
+++ py_awesome/core/assets.py
@@ -0,0 +1,6 @@
+from ..core.singleton import Singleton
+
+class Assets(Singleton):
+    ic_blank_check_box = 'py_awesome/assets/blank_check_box.png'
+    ic_selected_check_box = 'py_awesome/assets/selected_check_box.png'
+    font = 'py_awesome/assets/iciel_pony.ttf'
