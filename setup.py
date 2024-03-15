d169bbd5bc1f4dbd7e60b40b0def0dfb918f94fa
Cao Việt Đức <112269340+alexcao194@users.noreply.github.com>
2024-03-15 18:28:40 +0700

    .

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir = {"": "py_awesome"},
-    packages = setuptools.find_packages(where="py_awesome"),
+    packages = setuptools.find_packages(),
     python_requires = ">=3.6"
 )
