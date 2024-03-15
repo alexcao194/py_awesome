53629e30aa5aef71db481b59234e97e8db3cf0f3
Cao Việt Đức <112269340+alexcao194@users.noreply.github.com>
2024-03-15 17:07:26 +0700

    remove url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description = "short package description",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/alexcao194/py_awesome/",
+    # url = "",
     project_urls = {
         "Bug Tracker": "package issues URL",
     },
